9ec3ec1d5876112bbf6e44914e5028c050302eca
SrijDude3416 <66487875+SrijDude3416@users.noreply.github.com>
2024-11-14 20:47:20 -0800

    fixed location.hostname bug

--- src/assets/js/api/config.js
+++ src/assets/js/api/config.js
@@ -1,7 +1,7 @@
 export var pythonURI;
-if (location.hostname === "localhost") {
+if (window.location.hostname === "localhost") {
         pythonURI = "http://localhost:5000";
-} else if (location.hostname === "127.0.0.1") {
+} else if (window.location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:5000";
 } else {
         pythonURI =  "https://dudeamabobby.pythonanywhere.com";
